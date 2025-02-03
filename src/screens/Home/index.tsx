352e7a692de08dee7475234615a732ee25d4404c
ErikBezerra <erik_bezerra_silva@hotmail.com>
2025-02-03 18:19:10 -0300

    AA-00: funcionou no celular hehe

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -12,7 +12,7 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#f50',
     alignItems: 'center',
   },
 });
